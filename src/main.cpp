79fdce75ec74bc02532116149d89fa023190e9aa
duytap <duyp7916.2@gmail.com>
2026-02-09 12:23:33 +0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,150 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <openssl/sha.h>
+#include <iomanip>
+#include <ctime>
+#include <vector>
+#include "json.hpp"
+
+using namespace std;
+using json = nlohmann::json;
+
+
+class Block
+{
+private:
+    int index;
+    long long nonce;
+    time_t timest;
+    string diff;
+    string prev_hash, curr_hash, data;
+public:
+    Block(int ind, string prev, string curr, string d, string difficuty, time_t timestamp, long long numberUsedOnce)
+    {
+        index = ind;
+        prev_hash = prev;
+        data =  d;
+        curr_hash = curr;
+        diff = difficuty;
+        timest = timestamp;
+        nonce = numberUsedOnce;
+    }
+    string get_curr_hash()
+    {
+        return curr_hash;
+    }
+    string calc_hash() {
+        unsigned char hash[SHA256_DIGEST_LENGTH];
+        string aldata = to_string(index) + prev_hash + data + to_string(timest) + to_string(nonce) + diff;
+        SHA256((unsigned char*)aldata.c_str(), aldata.length(), hash);
+	    stringstream fina_hash;
+	    for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) fina_hash << hex << setw(2) << setfill('0') << (int)hash[i];
+
+        return fina_hash.str();
+    }
+    bool mine_block() {
+        curr_hash = calc_hash();
+        while (curr_hash.substr(0, diff.size()) != diff) {
+            ++nonce;
+            curr_hash = calc_hash();
+        }
+        return true;
+    }
+    string get_prev_hash()
+    {
+        return prev_hash;
+    }
+    string get_data()
+    {
+        return data;
+    }
+    string get_diff() {
+        return diff;
+    }
+    time_t get_time()
+    {
+        return timest;
+    }
+    int get_ind() {
+        return index;
+    }
+    long long get_nonce() {
+        return nonce;
+    }
+};
+
+class Blockchain
+{
+private:
+    vector<Block> chain;
+
+public:
+    bool createGenesisBlock()
+    {
+        chain.emplace_back(0, "Genesis Block", "", "The Beginning", "0f0c", time(nullptr), 0);
+        return true;
+    }
+    bool newBlock(string curr, string d, string difficuty, time_t timestamp, long long numberUsedOnce) {
+        chain.emplace_back(chain.size(), chain[chain.size() - 1].get_curr_hash(), curr, d, difficuty, timestamp, numberUsedOnce);
+        return true;
+    }
+    bool validatchain() {
+        for (int i = 1; i < chain.size(); ++i) {
+            if (chain[i].get_prev_hash() != chain[i - 1].get_curr_hash()) return false;
+        }
+        return true;
+    }
+    bool coutAll() {
+        for (int i = 0; i < chain.size(); ++i) {
+            cout << "index: " << chain[i].get_ind() << '\n';
+            cout << "prev: " << chain[i].get_prev_hash() << '\n';
+            cout << "curr: " << chain[i].get_curr_hash() << '\n';
+            cout << "data: " << chain[i].get_data() << '\n';
+            cout << "diff: " << chain[i].get_diff() << '\n';
+            time_t timed = time(nullptr);
+            cout << "time: " << chain[i].get_time() << " " << ctime(&timed);
+            cout << "nonce: " << chain[i].get_nonce() << '\n';
+        }
+        return true;
+    }
+    bool mine(int i = -1) {
+        if (i < 0) i = chain.size() - 1;
+        chain[i].mine_block();
+        return true;
+    }
+};
+
+int main()
+{
+    // ios::sync_with_stdio(false);
+    // cin.tie(nullptr);
+    // cout.tie(nullptr);
+    Blockchain chainLeader;
+    fstream blockFile("blocks.json");
+    if (blockFile.is_open() == false) {
+        system("touch blocks.json");
+    }
+        chainLeader.createGenesisBlock();
+    // json blockJSon = json::parse(blockFile);
+    string inp;
+    while (inp != "out") {
+        cout << "comm&>>";
+        cin >> inp;
+        if (inp == "new") {
+            cout << "data: ";
+            string data;
+            cin >> data;
+            cout << "diff: ";
+            string diff;
+            cin >> diff;
+            chainLeader.newBlock(" ", data, diff, time(nullptr), 0);
+        }
+        if (inp == "ls") chainLeader.coutAll();
+        if (inp == "mine") chainLeader.mine();
+    }
+    
+    return 0;
+}
+
